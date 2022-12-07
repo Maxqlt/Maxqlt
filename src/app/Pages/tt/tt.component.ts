13d70333b16b833fbf6753d255647b76ee317d81
Max Stöber <max.stoeber@e-domizil.de>
2022-12-07 12:09:16 +0100

    add telltale page

---
+++ src/app/Pages/tt/tt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tt',
+  templateUrl: './tt.component.html',
+  styleUrls: ['./tt.component.css']
+})
+export class TTComponent {
+
+}
